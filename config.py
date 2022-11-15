164ab18fd43b2a378b5149852a58dfd3c0e21d53
Cristhian Sempertegui <64335444+cristhiansemper@users.noreply.github.com>
2022-11-15 18:34:11 -0500

    int port

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import environ
 
 class Config():
     DB_HOST = environ.get("DB_HOST")
-    DB_PORT = environ.get("DB_PORT")
+    DB_PORT = int(environ.get("DB_PORT"))
     DB_USER = environ.get("DB_USER")
     DB_PASSWORD = environ.get("DB_PASSWORD")
     DB_NAME = environ.get("DB_NAME")
