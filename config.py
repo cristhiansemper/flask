e2404afd473c038afc6111189e51d0895d1ac6d9
Cristhian Sempertegui <64335444+cristhiansemper@users.noreply.github.com>
2022-11-15 17:16:17 -0500

    dotenv

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from os import environ
+
 class Config():
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
-    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = environ.get("DB_HOST")
+    DB_PORT = environ.get("DB_PORT")
+    DB_USER = environ.get("DB_USER")
+    DB_PASSWORD = environ.get("DB_PASSWORD")
+    DB_NAME = environ.get("DB_NAME")
 
 
 class SecretKey():
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY")
 
 
 class Host():
